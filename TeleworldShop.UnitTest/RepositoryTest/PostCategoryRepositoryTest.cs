0c060fd4fd42e4788c054eaadfd5c78ceabd4eaf
TienNM <tiennm1999@outlook.com.vn>
2020-10-11 11:49:55 +0700

    Add samples unit test for post category

---
+++ TeleworldShop.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeleworldShop.Data.Infrastructure;
+using TeleworldShop.Data.Repositories;
+using TeleworldShop.Model.Models;
+
+namespace TeleworldShop.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IUnitOfWork unitOfWork;
+        IPostCategoryRepository postCategoryRepository;
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            postCategoryRepository = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            var list = postCategoryRepository.GetAll().ToList();
+            Assert.AreEqual(3, list.Count());
+        }
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory postCategory = new PostCategory();
+            postCategory.Name = "Test category";
+            postCategory.Alias = "Test-category";
+            postCategory.Status = true;
+            var result = postCategoryRepository.Add(postCategory);
+            unitOfWork.Commit();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Id);
+         
+        }
+    }
+}
---
+++ TeleworldShop.UnitTest/ServiceTest/PostCategoryServiceTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using TeleworldShop.Data.Infrastructure;
+using TeleworldShop.Data.Repositories;
+using TeleworldShop.Model.Models;
+using TeleworldShop.Service;
+
+namespace TeleworldShop.UnitTest.ServiceTest
+{
+    [TestClass]
+    public class PostCategoryServiceTest
+    {
+        private Mock<IPostCategoryRepository> mockRepository;
+        private Mock<IUnitOfWork> mockUnitOfWork;
+        private PostCategoryService postCategoryService;
+        private List<PostCategory> postCategories;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mockRepository = new Mock<IPostCategoryRepository>();
+            mockUnitOfWork = new Mock<IUnitOfWork>();
+            postCategoryService = new PostCategoryService(mockRepository.Object, mockUnitOfWork.Object);
+            postCategories = new List<PostCategory>()
+            {
+                new PostCategory() { Id=1,Name="DM1",Status=true},
+                new PostCategory() { Id = 2, Name = "DM2", Status = true },
+                new PostCategory() { Id = 3, Name = "DM3", Status = true },
+            };
+        }
+
+        [TestMethod]
+        public void PostCategory_Service_GetAll()
+        {
+            //Set up method
+            mockRepository.Setup(m => m.GetAll(null)).Returns(postCategories);
+            //Call action
+            var result = postCategoryService.GetAll() as List<PostCategory>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count);
+        }
+
+        [TestMethod]
+        public void PostCategory_Service_Create()
+        {
+            PostCategory postCategory = new PostCategory();
+            postCategory.Name = "Test";
+            postCategory.Alias = "TEST";
+            postCategory.Status = true;
+            mockRepository.Setup(m => m.Add(postCategory)).Returns((PostCategory p) =>
+              {
+                  p.Id = 1;
+                  return p;
+              });
+            var result = postCategoryService.Add(postCategory);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Id);
+        }
+    }
+}
