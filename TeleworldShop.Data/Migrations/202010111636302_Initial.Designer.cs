3563ff35a8c21846002ee76fc85e4e47e0cdf90a
TienNM <tiennm1999@outlook.com.vn>
2020-10-13 19:55:25 +0700

    Configure ASP.NET Identity

--- TeleworldShop.Data/Migrations/202010111636302_Initial.Designer.cs
+++ TeleworldShop.Data/Migrations/202010111636302_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace TeleworldShop.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.4")]
-    public sealed partial class AddErrorTable : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(AddErrorTable));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "202010110939042_AddErrorTable"; }
+            get { return "202010111636302_Initial"; }
         }
         
         string IMigrationMetadata.Source
