467b880d70e9ba538271da6d6a17038ec9c44617
TienNM <tiennm1999@outlook.com.vn>
2020-10-14 11:07:45 +0700

    try services in angularjs

---
+++ TeleworldShop.Web/Scripts/spa/app.js
@@ -0,0 +1,24 @@
+﻿var myApp = angular.module("myModule", []);
+
+myApp.controller("schoolController", schoolController);
+myApp.service("Validator", Validator);
+schoolController.$inject = ["$scope", "Validator"];
+function schoolController($scope, Validator) {
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNumber($scope.num);
+    }
+    $scope.num = 1;
+}
+function Validator($window) {
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(n) {
+        if (n % 2 == 0) {
+            return "This is even";
+        }
+        else {
+            return "This is odd";
+        }
+    }
+}
