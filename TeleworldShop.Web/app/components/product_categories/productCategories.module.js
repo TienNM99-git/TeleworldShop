303391835abfa5b4f51cde2774b9cf5d93c3125a
TienNM <tiennm1999@outlook.com.vn>
2020-10-16 21:59:34 +0700

    update

--- TeleworldShop.Web/app/components/product_categories/productCategories.module.js
+++ TeleworldShop.Web/app/components/product_categories/productCategories.module.js
@@ -4,31 +4,32 @@
     angular.module('teleworldshop.product_categories', ['teleworldshop.common']).config(config);
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
     function config($stateProvider, $urlRouterProvider) {
-        var states = [
-            {
-                name: 'product_categories',
-                url: '/product_categories',
-                templateUrl: '/app/components/products/productCategoryListView.html',
-                controller: 'productCategoryListController'
-            }
-            //{
-            //    name: 'product_add',
-            //    url: '/product_add',
-            //    templateUrl: '/app/components/products/productAddView.html',
-            //    controller: 'productAddController'
-            //},
-            //{
-            //    name: 'product_edit',
-            //    url: '/product_edit',
-            //    templateUrl: '/app/components/products/productEditView.html',
-            //    controller: 'productEditController'
-            //}
-        ];
-        states.forEach((state) => $stateProvider.state(state));
-        //$stateProvider.state('products', {
-        //    url: "/products",
-        //    templateUrl: "/app/components/products/productListView.html",
-        //    controller: "productListController"
+        //var states = [
+        //    {
+        //        name: 'product_categories',
+        //        url: '/product_categories',
+        //        templateUrl: '/app/components/products/productCategoryListView.html',
+        //        controller: 'productCategoryListController'
+        //    }
+        //    {
+        //        name: 'product_add',
+        //        url: '/product_add',
+        //        templateUrl: '/app/components/products/productAddView.html',
+        //        controller: 'productAddController'
+        //    },
+        //    {
+        //        name: 'product_edit',
+        //        url: '/product_edit',
+        //        templateUrl: '/app/components/products/productEditView.html',
+        //        controller: 'productEditController'
+        //    }
+        //];
+        //states.forEach((state) => $stateProvider.state(state));
+        $stateProvider.state('product_categories', {
+            url: "/product_categories",
+            templateUrl: "app/components/product_categories/productCategoryListView.html",
+            controller: "productCategoryListController"
+        });
         //}).state('product_add', {
         //    url: "/product_add",
         //    templateUrl: "/app/components/products/productAddView.html",
--- TeleworldShop.Web/app/components/product_categories/productCategoryListController.js
+++ TeleworldShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,6 +1,22 @@
-﻿(function (app) {
+﻿
+(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
-    function productCategoryListController() {
 
+    productCategoryListController.$inject = ['$scope', 'apiService'];
+
+    function productCategoryListController($scope, apiService) {
+        $scope.productCategories = [];
+
+        $scope.getProductCagories = getProductCagories;
+
+        function getProductCagories() {
+            apiService.get('/api/productcategory/getallparents', null, function (result) {
+                $scope.productCategories = result.data;
+            }, function () {
+                console.log('Load Product Categories failed!');
+            });
+        }
+
+        $scope.getProductCagories();
     }
 })(angular.module('teleworldshop.product_categories'));
