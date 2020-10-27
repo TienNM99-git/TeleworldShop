0e264966c42000591c6c4e06e5b767964ddb5200
TienNM <tiennm1999@outlook.com.vn>
2020-10-27 10:16:26 +0700

    Using toastr for creating notifications

--- TeleworldShop.Web/app/components/product_categories/productCategoryListController.js
+++ TeleworldShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,9 +1,9 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService'];
+    productCategoryListController.$inject = ['$scope', 'apiService','notificationService'];
 
-    function productCategoryListController($scope, apiService) {
+    function productCategoryListController($scope, apiService, notificationService) {
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
@@ -24,6 +24,12 @@
                 }
             }
             apiService.get('/api/productcategory/getall', config, function (result) {
+                if (result.data.TotalCount == 0) {
+                    notificationService.displayWarning('No record found');
+                }
+                else {
+                    notificationService.displaySuccess('Found ' + result.data.TotalCount + (result.data.TotalCount == 1 ? ' record!!!':' records!!!'));
+                }
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
