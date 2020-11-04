1c7e8a2183a79e2dbad7b0dc653e665384855d5f
TienNM <tiennm1999@outlook.com.vn>
2020-11-04 16:14:39 +0700

    Add delete & delete multiple categories function

--- TeleworldShop.Web/app/components/product_categories/productCategoryListController.js
+++ TeleworldShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,16 +1,79 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService','notificationService'];
+    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService', '$ngBootbox', '$filter'];
 
-    function productCategoryListController($scope, apiService, notificationService) {
+    function productCategoryListController($scope, apiService, notificationService, $ngBootbox, $filter) {
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
         $scope.keyword = '';
         $scope.getProductCategories = getProductCategories;
-
         $scope.search = search;
+
+        $scope.deleteProductCategory = deleteProductCategory;
+        $scope.selectAll = selectAll;
+
+        $scope.deleteMultiple = deleteMultiple;
+
+        function deleteMultiple() {
+            var listId = [];
+            $.each($scope.selected, function (i, item) {
+                listId.push(item.Id);
+            });
+            var config = {
+                params: {
+                    checkedProductCategories: JSON.stringify(listId)
+                }
+            }
+            apiService.del('api/productcategory/deletemulti', config, function (result) {
+                notificationService.displaySuccess('Successful delete ' + result.data + ' records !!!');
+                search();
+            }, function (error) {
+                notificationService.displayError('Delete failed');
+            });
+        }
+
+        $scope.isAll = false;
+        function selectAll() {
+            if ($scope.isAll === false) {
+                angular.forEach($scope.productCategories, function (item) {
+                    item.checked = true;
+                });
+                $scope.isAll = true;
+            } else {
+                angular.forEach($scope.productCategories, function (item) {
+                    item.checked = false;
+                });
+                $scope.isAll = false;
+            }
+        }
+
+        $scope.$watch("productCategories", function (n, o) {
+            var checked = $filter("filter")(n, { checked: true });
+            if (checked.length) {
+                $scope.selected = checked;
+                $('#btnDelete').removeAttr('disabled');
+            } else {
+                $('#btnDelete').attr('disabled', 'disabled');
+            }
+        }, true);
+
+        function deleteProductCategory(id) {
+            $ngBootbox.confirm('Are you sure that you want to delete?').then(function () {
+                var config = {
+                    params: {
+                        id: id
+                    }
+                }
+                apiService.del('api/productcategory/delete', config, function () {
+                    notificationService.displaySuccess('Delete successful !!!');
+                    search();
+                }, function () {
+                    notificationService.displayError('Delete failed !!!');
+                })
+            });
+        }
         function search() {
             getProductCategories();
         }
--- TeleworldShop.Web/app/shared/modules/teleworldshop.common.js
+++ TeleworldShop.Web/app/shared/modules/teleworldshop.common.js
@@ -1,3 +1,3 @@
 ﻿(function () {
-    angular.module('teleworldshop.common', ['ui.router'])
+    angular.module('teleworldshop.common', ['ui.router','ngBootbox'])
 })();
