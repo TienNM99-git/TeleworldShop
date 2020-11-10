a5056a3f7341470dfb32baeb44272027de69d1e5
TienNM <tiennm1999@outlook.com.vn>
2020-11-10 22:07:18 +0700

    Bingding footers

--- TeleworldShop.Web/Controllers/HomeController.cs
+++ TeleworldShop.Web/Controllers/HomeController.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TeleworldShop.Model.Models;
+using TeleworldShop.Service;
+using TeleworldShop.Web.Mappings;
+using TeleworldShop.Web.Models;
 
 namespace TeleworldShop.Web.Controllers
 {
     public class HomeController : Controller
     {
+        IProductCategoryService _productCategoryService;
+        ICommonService _commonService;
+        public HomeController(IProductCategoryService productCategoryService, ICommonService commonService)
+        {
+            _productCategoryService = productCategoryService;
+            _commonService = commonService;
+        }
         public ActionResult Index()
         {
             return View();
@@ -29,7 +41,10 @@ namespace TeleworldShop.Web.Controllers
         [ChildActionOnly]
         public ActionResult Footer()
         {
-            return PartialView();
+            var footerModel = _commonService.GetFooter();
+            var mapper = new Mapper(AutoMapperConfiguration.Configure());
+            var footerViewModel = mapper.Map<Footer,FooterViewModel>(footerModel);
+            return PartialView(footerViewModel);
         }
 
         [ChildActionOnly]
@@ -41,7 +56,10 @@ namespace TeleworldShop.Web.Controllers
         [ChildActionOnly]
         public ActionResult Category()
         {
-            return PartialView();
+            var model = _productCategoryService.GetAll();
+            var mapper = new Mapper(AutoMapperConfiguration.Configure());
+            var listProductCategoryViewModel = mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
+            return PartialView(listProductCategoryViewModel);
         }
     }
 }
