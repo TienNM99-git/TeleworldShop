faf01ae43bbd3299b8c571e1e3412482ffc1e1cd
TienNM <tiennm1999@outlook.com.vn>
2020-12-27 13:50:04 +0700

    update ui for login/register form

--- TeleworldShop.Web/App_Start/BundleConfig.cs
+++ TeleworldShop.Web/App_Start/BundleConfig.cs
@@ -19,7 +19,17 @@ namespace TeleworldShop.Web
                  "~/Assets/admin/libs/jquery-validation/additional-methods.min.js",
                  "~/Assets/client/js/common.js"
                 ));
-
+            bundles.Add(new ScriptBundle("~/js/forms").Include(
+                "~/Assets/admin/libs/jquery/jquery.min.js",
+                "~/Assets/admin/libs/bootstrap/js/bootstrap.bundle.min.js",
+                "~/Assets/admin/js/adminlte.min.js"
+               ));
+            bundles.Add(new StyleBundle("~/css/advanced")
+               .Include("~/Assets/admin/css/adminlte.min.css", new CssRewriteUrlTransform())
+               .Include("~/Assets/admin/libs/fontawesome-free/css/all.min.css", new CssRewriteUrlTransform())
+               .Include("~/Assets/admin/libs/icheck-bootstrap/icheck-bootstrap.min.css", new CssRewriteUrlTransform())
+               .Include("~/Assets/client/css/custom.css", new CssRewriteUrlTransform())
+               );
             bundles.Add(new StyleBundle("~/css/base")
                 .Include("~/Assets/client/css/bootstrap.css", new CssRewriteUrlTransform())
                 .Include("~/Assets/client/font-awesome-4.6.3/css/font-awesome.css", new CssRewriteUrlTransform())
