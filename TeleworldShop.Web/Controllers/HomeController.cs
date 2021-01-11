34fee650e80514344cd4bd3ffd2d2bace3e19a6c
TienNM <tiennm1999@outlook.com.vn>
2021-01-11 10:50:34 +0700

    Add minor features

--- TeleworldShop.Web/Controllers/HomeController.cs
+++ TeleworldShop.Web/Controllers/HomeController.cs
@@ -65,6 +65,23 @@ namespace TeleworldShop.Web.Controllers
 
             return View();
         }
+        public ActionResult SetCulture()
+        {
+            string culture = "";
+            culture = CultureHelper.GetImplementedCulture(culture);
+            // Save culture in a cookie
+            HttpCookie cookie = Request.Cookies["_culture"];
+            if (cookie != null)
+                cookie.Value = culture;   // update cookie value
+            else
+            {
+                cookie = new HttpCookie("_culture");
+                cookie.Value = culture;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+            Response.Cookies.Add(cookie);
+            return RedirectToAction("Index");
+        }
         [ChildActionOnly]
         [OutputCache(Duration = 3600)]
         public ActionResult Footer()
