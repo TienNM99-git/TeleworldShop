cb75605400c6894b2c116f93a190aa9f46bb249b
TienNM <tiennm1999@outlook.com.vn>
2021-07-04 19:05:35 +0700

    update package

--- TeleworldShop.Web/App_Start/Startup.Auth.cs
+++ TeleworldShop.Web/App_Start/Startup.Auth.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
-using Microsoft.Owin.Host.SystemWeb;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
